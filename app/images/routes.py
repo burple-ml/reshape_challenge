07bcdb4b43a6ff7f7940fbce3499e629dea554ed
burple-ml <raghav.patne1@gmail.com>
2024-05-03 13:26:37 +0200

    added some more docs

--- app/images/routes.py
+++ app/images/routes.py
@@ -11,6 +11,11 @@ router = APIRouter(prefix='/v1')
 async def image_crop(image_data: dict = Depends(validate_image),
                      width: int = Query(..., description="Width of the image, has to be greater than 0 and less than 1000. Padding will be added in this dimension if your image is smaller", gt=0, le=1000), 
                      height: int = Query(..., description="Height of the image, has to be greater than 0 and less than 1000. Padding will be added in this dimension if your image is smaller", gt=0, le=1000)):
+    """ this route accepts a form data body, and 2 query parameters width and height which should be int, or else you will get a 422
+
+        Returns: 
+        the cropped image as html. Please download the html and save it in a .html file and open it in your browswer to see the cropped image.
+    """
 
     # pass to controller
     cropped_image_data = ImageHandler(image_data=image_data['image_data'],
@@ -24,15 +29,15 @@ async def image_crop(image_data: dict = Depends(validate_image),
 
 @router.post("/difference")
 async def image_difference(image_list: list = Depends(validate_multiform_data)):
-    """ this route accepts 2 images that you have to upload. 
+    """ this route strictly accepts 2 images that you have to upload. 
         it accepts multiform data, and only accept image/png and image/jpeg for each
         form item. 
+
+        This route has some issues in this version, we are working on it. 
 
         Returns: 
         an average cosine similarity score on a small sample of the input images, 
         which is the first 10 pixel intensities on each channel (r,g,b)
-        Due to some library constraints,
-        that solves for divide by zero exceptions, perfect 1 is a hard 
     """
     image1 = ImageHandler(image_data=image_list[0]['image_data'],
                           width=0, height=0,
@@ -45,6 +50,12 @@ async def image_difference(image_list: list = Depends(validate_multiform_data)):
 
 @router.post("/hash")
 async def image_hash(image_data: dict = Depends(validate_image)):
+    """ this route strictly accepts 1 image and computes the average hash value
+
+        Returns: 
+        returns the average hash value of size 8
+    """
+
     hash_value = ImageHandler(image_data=image_data['image_data'],
                           width=0, height=0,
                           content_type=image_data['content_type']).compute_average_hash()
